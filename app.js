0ff8609982c08beda3a03e2f8718d4ea426dec1a
Edson Batista <henrique.ti1196@gmail.com>
2019-02-02 12:04:27 -0200

    Add CORS policy

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var app         = require('connect')()
+var app = require('connect')()
 var serveStatic = require('serve-static')
+var cors = require('cors')
 
+app.use(cors())
 app.use(serveStatic('public'))
 
 console.log(' ➜   Open: http://localhost:7007')
