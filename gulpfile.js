73e33e7b41c5b97e3424a6ac77fb4cb8c5c670c1
Edson Batista <henrique.ti1196@gmail.com>
2019-02-03 22:26:03 -0200

    configure gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,25 @@
 const gulp = require('gulp');
+const {parallel} = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const autoprefixer = require('gulp-autoprefixer');
 
-// gulp.task('default', () => {
-//   return gulp.src('public/javascripts/*.js')
-//     .pipe(babel())
-//     .pipe(uglify())
-//     .pipe(gulp.dest('public/dist'))
-// });
+function buildJs() {
+  return gulp.src('public/javascripts/*.js')
+    .pipe(babel({
+      presets: ['@babel/env'],
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist/javascript'))
+};
 
-gulp.task('default', () => {
+function buildCss() {
   return gulp.src('public/stylesheets/style.css')
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
     }))
-    .pipe(gulp.dest('public/dist/css'))
-});
+    .pipe(gulp.dest('public/dist/stylesheets'))
+};
+
+exports.default = parallel(buildJs, buildCss);
