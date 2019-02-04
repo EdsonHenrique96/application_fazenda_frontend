c953717c69e2667400012cdbd0a7718d17cc8a8e
Edson Henrique <30643630+EdsonHenrique96@users.noreply.github.com>
2019-02-04 15:02:57 -0200

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,39 @@
-const gulp = require('gulp');
-const {parallel, series} = require('gulp');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const autoprefixer = require('gulp-autoprefixer');
-const sass = require('gulp-sass');
+var gulp = require('gulp');
+var {parallel, series} = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 
-function buildJs() {
-  return gulp.src('public/javascripts/*.js')
-    .pipe(babel({
-      presets: ['@babel/env'],
-    }))
-    .pipe(uglify())
-    .pipe(gulp.dest('public/dist/javascript'))
+function buildJs(watch) {
+  var bundler = watchify(browserify('public/javascripts/main.js', { debug: true }).transform(babel));
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('main.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('public/dist/javascripts'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return buildJs(true);
 };
 
 sass.compiler = require('node-sass');
