54f0c93002f8e6d80bc3f82481c2a38c6508869e
edson batista <edson.batista@catho.com>
2019-02-13 15:04:25 -0200

    refactoring build ..

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
   entry: path.join(__dirname, '/src/assets/javascripts/index.js'),
   output: {
-    path: path.join(__dirname, 'public/dist/javascripts/'),
+    path: path.join(__dirname, 'public/dist/'),
     filename: 'main.js',
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ],
   module: {
     rules: [{
       test: /\.s?[ac]ss$/,
       use: [
+        MiniCssExtractPlugin.loader,
         'css-loader',
         'sass-loader',
-        'style-loader'
+        'postcss-loader'
       ]
     }]
   }
